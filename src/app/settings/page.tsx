287c9503fa1e82ef4c2b7e0b0baec38b9ae7e9b5
abrhame <ablakew7ab@gmail.com>
2025-03-29 15:05:32 +0300

    feat navigator

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,16 @@
+// implement settings page here
+
+import Navigation from "@/components/ui/navigation";
+
+const Settings = () => {
+  return (
+    <div>
+      <Navigation />
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1>Settings</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
