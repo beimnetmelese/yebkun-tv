fcbf2ebdc52d26edaad51220027d31cc56fc59c2
abrhame <ablakew7ab@gmail.com>
2025-03-31 21:50:49 +0300

    remove unused fonts in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Genos } from "next/font/google";
+import {  Genos } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 const genos = Genos({
   weight: ["100", "200", "300", "400", "500", "600", "700"],
