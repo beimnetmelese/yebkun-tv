516a7a275cc3672b9d8ae7e55fdb25a047f30e3f
abrhame <ablakew7ab@gmail.com>
2025-04-03 10:53:28 +0300

    add space between rows of movie list

--- src/app/login/_components/MovieList.tsx
+++ src/app/login/_components/MovieList.tsx
@@ -29,7 +29,7 @@ function MovieList() {
   ];
 
   return (
-    <div className="w-full h-full grid grid-rows-[1fr_1fr_1fr]">
+    <div className="w-full h-full grid grid-rows-[1fr_1fr_1fr] gap-[10px]">
       {/* First row - Infinite scroll left */}
       <div className="relative overflow-hidden rounded-[15px]">
         <div className="flex animate-scroll">
